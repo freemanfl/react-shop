ec7aff339f5d20e9ef960c2cb7b01fbfd2308ee1
Farid Mammadbayov <mm.farid@post.cz>
2023-12-23 22:18:52 +0100

    slider in progress. 4208

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import React from "react";
+import slider1 from "../assets/images/slider/1.jpg"
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: #e2afaf;
+  
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -24,7 +26,53 @@ const Arrow = styled.div`
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
 `;
+
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+
+`
+
+const Slide = styled.div`
+  width: 100vw; 
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${props => props.bg};
+`;
+
+const ImgContainer = styled.div`
+height: 100%;
+flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`
+
+const InfoContainer = styled.div`
+flex: 1;
+padding: 50px;
+`;
+
+const Title = styled.h1`
+ font-size: 70px;
+`
+const Desc = styled.p`
+  margin: 50px 0px;font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+const Button = styled.button`
+        padding: 10px;
+        font-size: 20px;
+        background-color: transparent;
+        cursor: pointer;
+`
+
 
 const Slider = () => {
   return (
@@ -32,6 +80,41 @@ const Slider = () => {
       <Arrow direction="left">
         <ArrowLeftOutlined />
       </Arrow>
+      <Wrapper>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src={slider1} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+
+        <Slide bg="fcf1ed">
+          <ImgContainer>
+            <Image src={slider1} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>WINTER SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+
+        <Slide bg="fbf0f4">
+          <ImgContainer>
+            <Image src={slider1} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>POPuLAR SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
+            <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+
+      </Wrapper>
       <Arrow direction="right">
         <ArrowRightOutlined />
       </Arrow>
